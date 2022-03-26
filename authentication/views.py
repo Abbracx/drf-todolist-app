d95566791bd0a54be59f5a94eb8e7972a657464e
Abbracx <tankoraphael@gmail.com>
2022-03-26 18:42:31 +0100

    Added user registration with email and password

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from authentication.serializers import RegisterSerializer
 
-# Create your views here.
+class RegisterView(GenericAPIView):
+
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+
+        serializer = self.serializer_class(data=request.data)
+        
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
