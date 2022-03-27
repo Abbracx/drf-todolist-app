5747944ee6f1f66ff4197bd2c89c2a84a6c538b7
Abbracx <tankoraphael@gmail.com>
2022-03-27 09:13:04 +0100

    Added pagination

--- todolistapp/settings.py
+++ todolistapp/settings.py
@@ -85,6 +85,9 @@ DATABASES = {
 }
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination','PAGE_SIZE':2
+}
 # REST_FRAMEWORK = {
 #     'DEFAULT_AUTHENTICATION_CLASSES': [
 #         'authentication.jwtauthenticate.JWTAuthentication'
