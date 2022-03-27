438e75b54e67d00a0c09348cbac6731d0580f41b
Abbracx <tankoraphael@gmail.com>
2022-03-27 08:49:32 +0100

    Added functionality for search, filter and ordering

--- todos/views.py
+++ todos/views.py
@@ -1,15 +1,22 @@
 # from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import filters
 from authentication.jwtauthenticate import JWTAuthentication
 from todos.serializers import TodoSerializer
 from todos.models import Todo
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ListCreateTodoAPIView(ListCreateAPIView):
     serializer_class = TodoSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter ]
+
+    # What to use to do the filter
+    filterset_fields = ['id','title','desc','is_completed']
+    search_fields = ['id','title','desc','is_completed']
+    ordering_fields = ['id','title','desc','is_completed']
 
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
